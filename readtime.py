e546f7654e4d8e1d5c59c89b6406356326fd6e28
Kaiapuni <kairoseguo@gmail.com>
2018-07-25 10:30:18 -0700

    Added comments to readtime.py and removed some test code.

--- readtime.py
+++ readtime.py
@@ -1,10 +1,13 @@
 #! /usr/bin/python3
 
+# readtime.py: a script which prints the ctime of a file in human-readable format.
+# ctime is not necessarily the creation date, but it's the closest linux has.
+
 import os
 from datetime import datetime
 
-created = os.stat('mickeydir.py').st_ctime
+# Pull the ctime of the file
+created = os.stat('filebydate.py').st_ctime
 
+# Print the creation date in human-readable format
 print(datetime.fromtimestamp(created))
-readable = str(datetime.fromtimestamp(created))
-print(readable[0: 10])
